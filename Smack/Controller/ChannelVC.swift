2ebfd6392b5859de04676dec22ffc4a0a36f1270
ivan996 <ivan.stanimirovic692gmail.com>
2020-04-26 18:00:18 +0200

    finished LoggedIn Interface

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -12,6 +12,7 @@ class ChannelVC: UIViewController {
 
     //Outlets
     @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var userImg: CircleImage!
     
     
     
@@ -19,7 +20,8 @@ class ChannelVC: UIViewController {
         super.viewDidLoad()
         //postavljamo sirinu menija
         self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
-        
+        //primanje obavestenja
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userdDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
@@ -27,4 +29,16 @@ class ChannelVC: UIViewController {
     }
     @IBAction func prepareForUnwund(segue:UIStoryboardSegue){}
 
+    @objc func userdDataDidChange(_ notif: Notification){
+        if AuthService.instance.isLoggedIN{
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            userImg.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        }else{
+            loginBtn.setTitle("Login", for: .normal)
+            userImg.image = UIImage(named: "manuProfileIcon")
+            userImg.backgroundColor = UIColor.clear
+        }
+    }
+    
 }
